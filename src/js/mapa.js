cd64d7aff11028be8f073efe3eda928769568f60
Leonardo Basualdo <lbasualdo88@gmail.com>
2024-04-04 19:36:33 -0300

    first commit

---
+++ src/js/mapa.js
@@ -0,0 +1,21 @@
+if(document.querySelector('#mapa')) {
+
+    const lat = 32.969793
+    const lng = -60.644698
+    const zoom = 16
+    
+
+
+    const map = L.map('mapa').setView([lat, lng], zoom);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(`
+            <h2 class="mapa__heading">DevWebCamp</h2>
+            <p class="mapa__texto">Centro de Convenciones de Los Ángeles</p>
+        `)
+        .openPopup();
+}
